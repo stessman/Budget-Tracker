cb0c0a17a120e53828cbbdb7d73cd9b68c831136
Scott T <scott.tessm@gmail.com>
2021-09-26 19:12:09 -0500

    Fixed up service-worker so application can be used offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,74 @@
 const FILES_TO_CACHE = [
     '/',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
     '/index.html',
-    '/'
+    '/styles.css',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
 
-]
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+
+  self.addEventListener('fetch', function(evt) {
+    if(evt.request.url.includes('/api/')) {
+        console.log('Service worker Fetch (data)', evt.request.url);
+
+        evt.respondWith(
+            caches.open(STATIC_CACHE).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if(response.status ===200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+        );
+        return;
+    }
+
+    evt.respondWith(
+        caches.open(RUNTIME_CACHE).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
+  
